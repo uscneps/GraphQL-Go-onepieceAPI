fefa81f02b5ac3bf3c963622b227deee4347bbbf
Matteo Leonesi <matteo.leonesi@studenti.unicam.it>
2022-03-19 13:43:31 +0100

    feat : added the structure
    possibility to add a pirate, and search for all pirates

---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "onepiece/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+//go:generate go run github.com/99designs/gqlgen generate
+type Resolver struct{
+	pirates []*model.Pirate
+}
---
+++ graph/schema.resolvers.go
@@ -0,0 +1,35 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"onepiece/graph/generated"
+	"onepiece/graph/model"
+)
+
+func (r *mutationResolver) CreatePirate(ctx context.Context, input model.NewPirate) (*model.Pirate, error) {
+	pirate := &model.Pirate{
+		Name:   input.Name,
+		ID:     fmt.Sprintf("T%d", rand.Int()),
+		//User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+	r.pirates = append(r.pirates, pirate)
+	return pirate, nil
+}
+
+func (r *queryResolver) Pirates(ctx context.Context) ([]*model.Pirate, error) {
+	return r.pirates, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
